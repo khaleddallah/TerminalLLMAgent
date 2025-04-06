62f946397bc9d5bc470d9818b51e4fece91d25dc
ovi <ovi@ovihost.local>
2025-04-06 18:31:55 +0200

    Update handleQ.sh

--- handleQ.sh
+++ handleQ.sh
@@ -4,35 +4,22 @@ _GLOBAL_STDERR_LOG=$(mktemp)
 _SEARCH_WORD="?-"
 lock=false
 
-> "$_GLOBAL_STDERR_LOG"
 
 _bash_error_handler_detailed() {
   /home/ovi/env0/bin/python /home/ovi/software/BSagent/llm_bash_script_generator.py --error "$(cat $_GLOBAL_STDERR_LOG)" "$BASH_COMMAND"
-  > "$_GLOBAL_STDERR_LOG"
   exec 2>&1
   lock=false
 }
 trap '_bash_error_handler_detailed' ERR
 
 
-function pre_command() {
-  if [ "$lock" = false ]; then
-    # > "$_GLOBAL_STDERR_LOG"
-    exec 2> >(tee "$_GLOBAL_STDERR_LOG" >&2)
-    # > "$_GLOBAL_STDERR_LOG"
-    lock=true
-  fi
-}
-trap 'pre_command' DEBUG
-
-
-# Custom command-not-found handler
+# Custom command-not-found handler (override of a built in function)
 command_not_found_handle() {
     # Check if the command starts with _SEARCH_WORD
     cmd0=$*
     if [[ $cmd0 == $_SEARCH_WORD* ]]; then
-        query="${cmd0//$_SEARCH_WORD/}"
-        /home/ovi/env0/bin/python /home/ovi/software/BSagent/llm_bash_script_generator.py "$query"
+        cmd0="${cmd0//$_SEARCH_WORD/}"
+        /home/ovi/env0/bin/python /home/ovi/software/BSagent/llm_bash_script_generator.py "$cmd0"
         exec 2>&1
         return 0
     fi
@@ -44,10 +31,20 @@ command_not_found_handle() {
 }
 
 
-function post_command() {
-    > "$_GLOBAL_STDERR_LOG"
+function inturrupt0() {
     exec 2>&1
-    trap - SIGINT
 }
-# Trap the ERR signal to run the error_handler function
-trap 'post_command' SIGINT
+trap 'inturrupt0' SIGINT
+
+
+# IMPORTANT : it is important for this to be as the last
+#             because it cause problems in read errors if then
+#             the pre_command trap start before
+function pre_command() {
+  if [ "$lock" = false ]; then
+    exec 2> >(tee "$_GLOBAL_STDERR_LOG" >&2)
+    > "$_GLOBAL_STDERR_LOG"
+    lock=true
+  fi
+}
+trap 'pre_command' DEBUG
